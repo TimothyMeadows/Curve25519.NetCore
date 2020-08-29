db186d019c9346612eb8e2c0d9e8e07b60fe97bb
Timothy D Meadows II <timothy@timothymeadows.com>
2020-08-28 17:36:52 -0700

    Add Curve25519.NetCore

---
+++ Curve25519.NetCore.Examples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Curve25519.NetCore.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alicePrivate = Curve25519.CreateRandomPrivateKey();
+            var alicePublic = Curve25519.GetPublicKey(alicePrivate);
+
+            var bobPrivate = Curve25519.CreateRandomPrivateKey();
+            var bobPublic = Curve25519.GetPublicKey(bobPrivate);
+
+            var aliceShared = Curve25519.GetSharedSecret(alicePrivate, bobPublic);
+            var bobShared = Curve25519.GetSharedSecret(bobPrivate, alicePublic);
+            var equal = aliceShared.SequenceEqual(bobShared);
+
+            return;
+        }
+    }
+}
